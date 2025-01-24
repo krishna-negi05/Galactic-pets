981f08f7c4991d844bd009c3b76710a0108e4d3a
Krishna <krish1902n@gmail.com>
2025-01-25 03:10:38 +0530

    change localhost

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode:false,
-    images:{
-        domains:['localhost']
+    reactStrictMode: false,
+    images: {
+        domains: [
+            'localhost',
+            process.env.NEXT_PUBLIC_BACKEND_BASE_URL ? new URL(process.env.NEXT_PUBLIC_BACKEND_BASE_URL).hostname : 'galactic-pet-backend.onrender.com'
+        ]
     }
 };
 
